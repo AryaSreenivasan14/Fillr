192f66b5c0753a52a96e581c21bd9dd2c3b9e617
Arya Sreenivasan <aryasreenivasan14@gmail.com>
2021-04-06 10:47:52 +1000

    Create outlets for UI

--- SpaceGame/ViewController.swift
+++ SpaceGame/ViewController.swift
@@ -8,7 +8,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var startUI: UIView!
+    @IBOutlet weak var infoLabel: UILabel!
+    @IBOutlet weak var lifeLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
